faeb00d760dac0f95948420f4ac2da4a49f4b318
workwithraymond <workwithraymond01@gmail.com>
2024-08-08 20:40:00 -0400

    Add project files.

---
+++ LoginPage/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace LoginPage
+{
+    partial class SellBuyCar
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SellBuyCar));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(63, 74);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1174, 662);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(823, 616);
+            label1.Name = "label1";
+            label1.Size = new Size(288, 55);
+            label1.TabIndex = 1;
+            label1.Text = "Book A Flight";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(138, 616);
+            label2.Name = "label2";
+            label2.Size = new Size(281, 55);
+            label2.TabIndex = 2;
+            label2.Text = "Book A Hotel";
+            label2.Click += label2_Click;
+            // 
+            // SellBuyCar
+            // 
+            AutoScaleDimensions = new SizeF(12F, 31F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1386, 812);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Font = new Font("Segoe Print", 9F);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "SellBuyCar";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "WelcomePage";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ LoginPage/Form1.cs
@@ -0,0 +1,20 @@
+namespace LoginPage
+{
+    public partial class SellBuyCar : Form
+    {
+        public SellBuyCar()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
